8a9974ee09c205637ab9208e39edf768092b307f
jacobmcgreen <119445109+jacobmcgreen@users.noreply.github.com>
2023-09-28 17:58:12 -0400

    more tests

--- UnitTest.java
+++ UnitTest.java
@@ -13,12 +13,25 @@ public class UnitTest {
             if (test1.get(7) == 7){
                 System.out.println("Test 1 Passed");
             }
+            System.out.println(Arrays.toString(test1.toArray()));
             test1.remove(2);
             if (test1.get(2) == 3){
                 System.out.println("Test 2 Passed");
             }
+            System.out.println(Arrays.toString(test1.toArray()));
             if (test1.size() == 9){
                 System.out.println("Test 3 Passed");
+            }
+            int hmmm = 4;
+            System.out.println(Arrays.toString(test1.toArray()));
+            if (test1.indexOf(hmmm)==3){
+                System.out.println("Test 4 Passed");
+            }
+            if (test1.contains(8)==true){
+                System.out.println("Test 5 Passed");
+            }
+            if (test1.contains(2)==false){
+                System.out.println("Test 6 Passed");
             }
     }
 }
